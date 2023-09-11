d2e85de4b5ef5bed05a269079a8e5728e6279c3a
Sergey Dianov <sergik5849@gmail.com>
2023-09-11 20:04:45 +0300

    add game guess the number

---
+++ practice_textbook/guess_the_bumber/main.js
@@ -0,0 +1,23 @@
+"use strict"
+
+const input_1 = document.querySelector('.input-1');
+
+const button = document.querySelector('.button');
+const answer_block = document.querySelector('.game__answer-main');
+
+// random value
+const randomValue = Math.floor(Math.random() * 100) + 1
+console.log(randomValue)
+button.addEventListener('click', e => {
+    const a = parseFloat(input_1.value);
+
+    if (!isNaN(a)) {
+        if (a < randomValue) {
+            answer_block.textContent = `Введите число побольше`;
+        } else if (a > randomValue) {
+            answer_block.textContent = `Введите число поменьше`;
+        } else if (a === randomValue) {
+            answer_block.textContent = `Поздравляю, вы угадали число!`;
+        }
+    }
+})
