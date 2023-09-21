55f67a5f3181d4f04396d402ac06fe6d440d1ea5
Sergey Dianov <sergik5849@gmail.com>
2023-09-21 08:32:44 +0300

    hotfix

--- prime_textbook/block_484/tasks_10-12/main.js
+++ prime_textbook/block_484/tasks_10-12/main.js
@@ -6,74 +6,11 @@ let employees = [
 
 let uls = document.querySelector('.uls')
 
-// new employe
-let form = document.getElementById('form')
-
-form.addEventListener('submit', e => {
-    e.preventDefault()
-
-    let name = form.name.value
-    let age = form.age.value
-    let salary = form.salary.value
-
-    if (name.length > 0 && age.length > 0 && salary.length > 0) {
-        let new_employe = {}
-
-        new_employe.name = name;
-        new_employe.age = age;
-        new_employe.salary = salary;
-
-        employees.push(new_employe)
-
-        ul = document.createElement('ul')
-        for (const item in new_employe) {
-
-            let li = document.createElement('li')
-            let span = document.createElement('span')
-            span.textContent = new_employe[item]
-
-            li.appendChild(span)
-            ul.appendChild(li)
-            uls.appendChild(ul)
-
-            span.addEventListener('click', function func() {
-                let input = document.createElement('input');
-                input.value = span.textContent;
-
-                span.textContent = '';
-                span.appendChild(input);
-
-                input.addEventListener('blur', function () {
-                    span.textContent = this.value;
-                    span.addEventListener('click', func);
-                });
-
-                span.removeEventListener('click', func);
-            });
-
-            // remove button
-            let remove = document.createElement('a');
-            remove.href = '';
-            remove.textContent = 'remove';
-
-            remove.addEventListener('click', function (event) {
-                li.remove();
-                event.preventDefault();
-            });
-
-            li.appendChild(remove);
-        }
-    }
-
-})
-
-for (const employe of employees) {
-    ul = document.createElement('ul')
-    for (const item in employe) {
-
+function add_new_li(new_employe, uls, ul) {
+    for (const item in new_employe) {
         let li = document.createElement('li')
         let span = document.createElement('span')
-        span.textContent = employe[item]
+        span.textContent = new_employe[item]
 
         li.appendChild(span)
         ul.appendChild(li)
@@ -108,3 +45,32 @@ for (const employe of employees) {
     }
 }
 
+// new employe
+let form = document.getElementById('form')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    let name = form.name.value
+    let age = form.age.value
+    let salary = form.salary.value
+
+    if (name.length > 0 && age.length > 0 && salary.length > 0) {
+        let new_employe = {}
+
+        new_employe.name = name;
+        new_employe.age = age;
+        new_employe.salary = salary;
+
+        employees.push(new_employe)
+
+        ul = document.createElement('ul')
+        add_new_li(new_employe, uls, ul)
+    }
+
+})
+
+for (const employe of employees) {
+    ul = document.createElement('ul')
+    add_new_li(employe, uls, ul)
+}
